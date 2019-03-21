1f06a39e3f55d0dbaea2d121b4a13bde29386409
firedial <42674772+firedial@users.noreply.github.com>
2019-03-21 13:48:25 +0900

    環境変数の設定

--- db/init.go
+++ db/init.go
@@ -2,12 +2,14 @@ package db
 
 import (
     "database/sql"
-
     _ "github.com/go-sql-driver/mysql"
+
+    "github.com/firedial/midas-go/config"
 )
 
 func Init() *sql.DB {
-    db, err := sql.Open("mysql", "root:@/midas?parseTime=true")
+    dsn := config.DB_USER + ":" + config.DB_PASSWORD + "@/midas?parseTime=true"
+    db, err := sql.Open("mysql", dsn)
     if err != nil {
       panic(err.Error())
     }
