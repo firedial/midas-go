330497608d263d4fb9ae533986d1c120dd9ddb35
firedial <42674772+firedial@users.noreply.github.com>
2019-03-21 14:22:41 +0900

    複数属性が取れるようにした

--- controller/attribute.go
+++ controller/attribute.go
@@ -4,8 +4,12 @@ import(
     "github.com/firedial/midas-go/model"
 )
 
-func AttributeGet(attribute_name string, queries map[string][]string) model.Attribute {
-    id := queries["id"][0]
+func AttributeGet(attribute_name string, queries map[string][]string) []model.Attribute {
+    ids, isExist := queries["id"]
+    id := "";
+    if isExist {
+        id = ids[0]
+    }
     attribute := model.GetAttribute(attribute_name, id)
     return attribute
 }
--- model/attribute.go
+++ model/attribute.go
@@ -11,31 +11,50 @@ type Attribute struct {
     Group_id int `json:"group_id"`
 }
 
-func GetAttribute(attribute_name string, id string) Attribute {
-    var attribute Attribute
+func GetAttribute(attribute_name string, id string) []Attribute {
+    var attributes []Attribute
 
     db := db.Init();
     defer db.Close();
 
     where := "";
-    where = " WHERE id = " + string(id)
+    if id != "" {
+        where = " WHERE id = " + string(id)
+    }
 
-    err := db.QueryRow(`
-        SELECT
-            id,
-            name,
-            description,
-            group_id
-        FROM ` + attribute_name + where).Scan(
-        &(attribute.Id),
-        &(attribute.Name),
-        &(attribute.Description),
-        &(attribute.Group_id))
+    _ = where
+
+    stmt, err := db.Prepare("SELECT id, name, description, group_id FROM " + attribute_name)
+    if err != nil {
+        panic(err)
+    }
+    defer stmt.Close()
+
+    rows, err := stmt.Query()
+    if err != nil {
+        panic(err)
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var attribute Attribute
+
+        err := rows.Scan(
+            &(attribute.Id),
+            &(attribute.Name),
+            &(attribute.Description),
+            &(attribute.Group_id))
+        if err != nil {
+            panic(err)
+        }
+
+        attributes = append(attributes, attribute)
+    }
 
     if err != nil {
         panic(err.Error())
     }
-    return attribute
+    return attributes
 }
 
 
