dcfcd2e773d7fdef7ef70d23608ae7eb2684b773
firedial <42674772+firedial@users.noreply.github.com>
2019-03-21 13:13:40 +0900

    routing をいい感じにまとめた

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
     "github.com/gin-gonic/gin"
 
-    //"github.com/firedial/midas/db"
     "github.com/firedial/go-midas/controller"
     "github.com/firedial/go-midas/model"
 
@@ -12,22 +11,17 @@ import (
 func main() {
     r := gin.Default()
 
-    balance := r.Group("/api/v1/balance")
+    api := r.Group("/api/v1")
     {
-        balance.GET("/", func(c *gin.Context) { c.JSON(200, controller.BalanceGet(c.Request.URL.Query())) } )
-        // balance.POST("/", func(c *gin.Context) { c.JSON(200, controller.BalancePost(c.GetPostFormMap())) } )
-        balance.POST("/", func(c *gin.Context) { 
+        api.GET("/balance/", func(c *gin.Context) { c.JSON(200, controller.BalanceGet(c.Request.URL.Query())) } )
+        api.POST("/balance/", func(c *gin.Context) { 
             var balance model.Balance
             c.BindJSON(&balance)
             c.JSON(200, controller.BalancePost(balance)) } )
+        api.GET("/kind/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("kind", c.Request.URL.Query())) } )
+        api.GET("/purpose/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("purpose", c.Request.URL.Query())) } )
+        api.GET("/place/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("place", c.Request.URL.Query())) } )
     }
 
-    r.GET("/api/v1/kind/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("kind", c.Request.URL.Query())) } )
-    r.GET("/api/v1/purpose/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("purpose", c.Request.URL.Query())) } )
-    r.GET("/api/v1/place/", func(c *gin.Context) { c.JSON(200, controller.AttributeGet("place", c.Request.URL.Query())) } )
-    //r.GET("/", func(c *gin.Context) {
-    //    c.String(200, "Hello, World")
-    //})
-    //db.Init()
     r.Run()
 }
