93f2ad756c9b137384593f17db1781bdf89975f8
firedial <42674772+firedial@users.noreply.github.com>
2019-06-15 18:25:02 +0900

    json 用の名前を定義した

--- entity/balance.go
+++ entity/balance.go
@@ -3,13 +3,13 @@ package entity
 import "time"
 
 type Balance struct {
-	BalanceId int
-	Amount int
-	Item string
-	KindId int
-	PurposeId int
-	PlaceId int
-	Date time.Time
+	BalanceId int `json:"balance_id"`
+	Amount int `json:"amount"`
+	Item string `json:"item"`
+	KindId int `json:"kind_id`
+	PurposeId int `json:"purpose_id"`
+	PlaceId int `json:place_id"`
+	Date time.Time `json:"date"`
 }
 
 type Balances []Balance
