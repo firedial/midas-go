f3c1e25e290cbefcb4e0ed28536be579e1b8416a
firedial <42674772+firedial@users.noreply.github.com>
2019-06-22 17:40:51 +0900

    ダブルクォーテーションが抜けていた

--- entity/balance.go
+++ entity/balance.go
@@ -10,9 +10,9 @@ type Balance struct {
     BalanceId int `json:"balance_id"`
     Amount int `json:"amount"`
     Item string `json:"item"`
-    KindId int `json:"kind_id`
+    KindId int `json:"kind_id"`
     PurposeId int `json:"purpose_id"`
-    PlaceId int `json:place_id"`
+    PlaceId int `json:"place_id"`
     Date string `json:"date"`
 }
 
